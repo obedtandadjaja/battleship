e38094a9c015336aea07706d35d1960b40bc6278
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-04-11 13:49:13 -0400

    added channel string to game

--- db/migrate/20160409184009_create_games.rb
+++ db/migrate/20160409184009_create_games.rb
@@ -3,6 +3,7 @@ class CreateGames < ActiveRecord::Migration
     create_table :games do |t|
       t.string :name, null: false
       t.string :password
+      t.string :channel, null: false
       t.boolean :is_completed, default: false
       t.boolean :is_playing, default: false
       t.integer :num_players, null: false

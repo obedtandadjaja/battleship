ceb04ef5ad89b6fe210831d9716f57f22ca82230
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-04-11 13:50:45 -0400

    added play controller that handles all the web sockets stuff

---
+++ app/controllers/play_controller.rb
@@ -0,0 +1,35 @@
+class PlayController < WebsocketRails::BaseController
+  	
+  	def initialize_session
+    	# perform application setup here
+    	controller_store[:message_count] = 0
+  	end
+
+  	def guess
+		@user = current_user || guest_user
+		@game = Game.find(params[:game_id])
+		if @user.game.include? @game
+			@col = params[:col]
+			@row = params[:row]
+			@ship_cells = @game.ship.ship_cells
+			@player = GamePlayer.where(game_id: @game.id, user_id: @user.id)
+			@guesses = @player.guess
+			@ship_cells.each do |cell|
+				if cell.row == @row && cell.col == @col && cell.is_hit == false
+					cell.update_attributes(is_hit: true)
+					new_hits = {:ship_cells => @ship_cells.to_json, :guesses => @guesses}
+					broadcast_message :update_board, new_hits
+					return
+				end
+			end
+			@guess = Guess.create(row: @row, col: @col, is_hit: true, game_player_id: @player.id)
+			@player.guess << @guess
+			new_hits = {:ship_cells => @ship_cells.to_json, :guesses => @guesses.to_json}
+			broadcast_message :update_board, new_hits
+		else
+			flash[:notice] = "You have no permission!"
+			redirect_to '/'
+		end
+	end
+	
+end

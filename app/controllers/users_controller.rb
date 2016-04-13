c4059667bd59618836262a97112d7b30efccbb02
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-04-12 23:45:29 -0400

    replaced id with slug for finds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,21 @@ class UsersController < ApplicationController
 	end
 
 	def show
+		@user = User.friendly.find(params[:id])
+	end
+
+	def edit
+		@user = User.friendly.find(params[:id])
+	end
+
+	def update
 		@user = User.find(params[:id])
+		if @user.update_attributes(name: params[:user][:name], email: params[:user][:email])
+			redirect_to action: :show, id: @user.slug
+		else
+			flash[:alert] = @user.errors.full_messages
+			redirect_to action: :edit, id: @user.slug
+		end
 	end
 
 end

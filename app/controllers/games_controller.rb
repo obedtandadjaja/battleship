a44639dc9333d5a199ff826f1109ca4a38d25947
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-04-12 20:17:15 -0400

    create a random channel string

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,5 @@
 class GamesController < ApplicationController
 skip_before_filter :verify_authenticity_token, :only => [:guess]
-require 'securerandom'
 
 	def in_game_lobby
 		@game = Game.find(params[:id])
@@ -15,17 +14,16 @@ require 'securerandom'
 	end
 
 	def create
-		random_string = SecureRandom.base_64
+		require 'securerandom'
+		random_string = SecureRandom.base64
 		@game = Game.create(num_players: params[:game][:num_players], name: params[:game][:name],
-			password: params[:game][:password], type: params[:game][:type], channel: params[:game][:channel])
+			password: params[:game][:password], type: params[:game][:type], channel: random_string)
 		if @game.save
 			redirect_to "/games/in_game_lobby/#{@game.id}"
 		else
 			flash[:alert] = @game.errors.full_messages
 			redirect_to '/'
 		end
-	rescue ActiveRecord::StatementInvalid
-		flash[:notice] = @game.errors.full_messages
 	end
 
 	def destroy

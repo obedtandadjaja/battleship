0639e6da4fa51ed34ef5f51efd17ff1ed1e3aecd
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-04-23 23:04:38 -0400

    ship cells

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -16,7 +16,16 @@ class PlayController < WebsocketRails::BaseController
 		end
 
 		@user_ships = randomize_ship_cells(@game)
-		puts @user_ships.to_json
+		puts @user_ships
+		@user_ships.each do |key, value|
+			@player = GamePlayer.where(game_id: game_id, user_id: key).first
+			value.each do |i|
+				ship = Ship.create(game_player_id: @player.id, is_sunk: false)
+				i.each do |x|
+					ShipCell.create(ship_id: ship.id, row: x[0], column: x[1], is_hit: false)
+				end
+			end
+		end
 
 		# Send redirect broadcast
   		WebsocketRails[channel].trigger(:playgame, @game.slug)
@@ -27,21 +36,22 @@ class PlayController < WebsocketRails::BaseController
   		@game = game
   		@user_ships = Hash.new
   		@game_cells = Hash.new
-  		# @game.user.each do |user|
-  			@game_cells[1] = Array.new
-  			2.times do
+  		@game.user.each do |user|
+  			@user_ships[user.id] = Array.new
+  			2.times do |i|
+  				@user_ships[user.id][i] = Array.new
 	  			while(true)
 	  				ship_cell = random_ship
-	  				if (!@game_cells.key? random_ship[0]) && (!@game_cells.key? random_ship[1])
-	  					@game_cells[random_ship[0]] = true
-	  					@game_cells[random_ship[1]] = true
-	  					@game_cells[user.id].append random_ship[0]
-	  					@game_cells[user.id].append random_ship[1]
+	  				if (!@game_cells.key? ship_cell[0]) && (!@game_cells.key? ship_cell[1])
+	  					@game_cells[ship_cell[0]] = true
+	  					@game_cells[ship_cell[1]] = true
+	  					@user_ships[user.id][i] << ship_cell[0]
+	  					@user_ships[user.id][i] << ship_cell[1]
 	  					break
 	  				end
 	  			end
 	  		end
-  		# end
+  		end
 
   		return @user_ships
   	end
@@ -91,7 +101,7 @@ class PlayController < WebsocketRails::BaseController
   			end
   		end
 
-  		return [[first_row, first_col], [second_row, second_col]]
+  		return [[first_row.chr, first_col], [second_row.chr, second_col]]
   	end
 
   	def guess
--- test.rb
+++ test.rb
@@ -1,72 +1,19 @@
-def randomize_ship_cells
-  		# Randomize two 2x1 ships
-  		@user_ships = Hash.new
-  		@game_cells = Hash.new
-  		# @game.user.each do |user|
-  			@game_cells[1] = Array.new
-  			2.times do
-	  			while(true)
-	  				ship_cell = random_ship
-	  				if (!@game_cells.key? random_ship[0]) && (!@game_cells.key? random_ship[1])
-	  					@game_cells[random_ship[0]] = true
-	  					@game_cells[random_ship[1]] = true
-	  					@game_cells[1] << random_ship[0]
-	  					@game_cells[1] << random_ship[1]
-	  					break
-	  				end
-	  			end
-	  		end
-  		# end
+@user_ships = Hash.new
+@user_ships[5] = Array.new
+@user_ships[5][0] = Array.new
+@user_ships[5][0] << ["E", 9]
+@user_ships[5][0] << ["E", 10]
+@user_ships[5][1] = Array.new
+@user_ships[5][1] << ["D", 5]
+@user_ships[5][1] << ["C", 5]
 
-  		return @user_ships
-  	end
-
-  	def random_ship
-  		# randomize first column
-  		first_row = rand(65..75)
-  		first_col = rand(1..10)
-
-  		# second column must be a neighbor of the first column
-  		random1 = rand(2) == 1
-  		random2 = rand(2) == 1
-
-  		if random1
-  			# same column different row
-  			second_col = first_col
-  			second_row = -1
-  			if random2
-  				if(first_row-1 > 0)
-	  				second_row = first_row-1
-	  			elsif(first_row+1 < 11)
-	  				second_row = first_row+1
-	  			end
-  			else
-  				if(first_row+1 < 11)
-	  				second_row = first_row+1
-	  			elsif(first_row-1 > 0)
-	  				second_row = first_row-1
-	  			end
-  			end
-  		else
-  			# same row different column
-  			second_row = first_row
-  			second_col = -1
-  			if random2
-  				if(first_col-1 > 0)
-	  				second_col = first_col-1
-	  			elsif(first_col+1 < 11)
-	  				second_col = first_col+1
-	  			end
-  			else
-  				if(first_col+1 < 11)
-	  				second_col = first_col+1
-	  			elsif(first_col-1 > 0)
-	  				second_col = first_col-1
-	  			end
-  			end
-  		end
-
-  		return [[first_row.chr, first_col], [second_row.chr, second_col]]
-  	end
-
-puts random_ship
+@user_ships.each do |key, value|
+  # @player = GamePlayer.where(game_id: game_id, user_id: key).first
+  value.each do |i|
+    # ship = Ship.create(game_player_id: @player.id, is_sunk: false)
+    i.each do |x|
+      # ShipCell.create(ship_id: ship.id, row: x[0], column: x[1], is_hit: false)
+      puts "#{x[0]}, #{x[1]}"
+    end
+  end
+end

516d41ec1bf36156de9f258e310e8fba6346e8b9
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2016-04-25 10:58:26 -0400

    must be more than 1

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
 
 	validates :name, presence: true
 	validates :type, presence: true
-	validates :num_players, presence: true, numericality: {greater_than_or_equal_to: 1}
+	validates :num_players, presence: true, numericality: {greater_than_or_equal_to: 2}
 	validates :is_completed, default: false
 	validates :is_playing, default: false
 	validates :random, presence: true
